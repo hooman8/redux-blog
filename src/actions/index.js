5c11396183888ea23264a80ba3f669ee489e7e8e
Hooman Pegahmehr <hpegahmehr@gmail.com>
2022-05-22 16:46:55 -0400

    implementation of a memoized fetching for fetchUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => {
@@ -8,8 +9,11 @@ export const fetchPosts = () => {
 };
 
 // Action creator for fetching one user at the time
-export const fetchUser = (id) => async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({type: 'FETCH_USER', payload: response.data});
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
 };
 
+const _fetchUser = _.memoize(async(id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({type: 'FETCH_USER', payload: response.data});
+});
