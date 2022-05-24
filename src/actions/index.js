de05a1a423b6c5cfe9839a64e02c3ad85752ed7f
Hooman Pegahmehr <hpegahmehr@gmail.com>
2022-05-24 18:54:33 -0400

    add more comments about the memoize fetch user function

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,18 +10,20 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 };
 
 export const fetchPosts = () => {
-    return async function(dispatch){
+    return async function (dispatch) {
         const response = await jsonPlaceholder.get('/posts');
-        dispatch({type: 'FETCH_POSTS', payload: response.data});            
-        };   
+        dispatch({ type: 'FETCH_POSTS', payload: response.data });
+    };
 };
 
 // Action creator for fetching one user at the time
 
-export const fetchUser = (id) =>  async dispatch => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({type: 'FETCH_USER', payload: response.data});
+    dispatch({ type: 'FETCH_USER', payload: response.data });
 };
+
+// the following code belongs to the approach that memoize the fetch user function in order to prevent the same call for a repeating user
 
 // export const fetchUser = (id) => dispatch => {
 //     _fetchUser(id, dispatch);
